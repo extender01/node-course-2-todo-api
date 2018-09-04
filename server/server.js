640dd7d0d1544c415b5230b0dc903689c49264cd
extender01 <vit@krajicek.org>
2018-09-04 14:15:24 +0200

    Add DELETE /todos/:id route'

--- server/server.js
+++ server/server.js
@@ -56,7 +56,24 @@ app.get('/todos/:id', (req, res) => {
 })
 
 
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
 
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((hledaneTodo) => {
+        if (!hledaneTodo) {
+            return res.status(404).send("not found");
+        }
+
+        res.send({deleted: hledaneTodo});
+    }).catch((e) => {
+        res.status(404).send("error")
+    });
+
+});
 
 
 
