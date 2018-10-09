260cfcd7aae6a0d32219c935e8c39583939b347f
Vit Krajicek <vit@krajicek.org>
2018-10-09 12:16:10 +0200

    hash user password

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 
 // {
@@ -86,7 +87,21 @@ UserSchema.statics.findByToken = function (token) {
     })
 };
 
+UserSchema.pre('save', function (next) {
+  let user = this;
 
+  //kontrola jestli bylo zmeneno heslo, kdyz ne (treba zmeneno neco jineho) tak se nic nedela aby se nehashovalo uz jednou hashovane heslo
+  if(user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      })
+    })
+  } else {
+    next();
+  }
+});
 
 
 
